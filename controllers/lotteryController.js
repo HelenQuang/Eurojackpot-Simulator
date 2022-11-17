abb6a59f09206bb34f7674c463a30c851d7a4ac4
Helen Quang <94285120+HelenQuang@users.noreply.github.com>
2022-11-17 17:53:49 +0200

    userController

---
+++ controllers/lotteryController.js
@@ -0,0 +1,1 @@
+const Lottery = require("../models/lotteryModel");
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,60 +1,16 @@
-const fs = require("fs");
+const User = require("../models/userModel");
+const factory = require("./handlerFactory");
 
-const users = JSON.parse(
-  fs.readFileSync(`${__dirname}/../data/users.json`, "utf-8")
-);
-
-exports.getAllUsers = (req, res) => {
-  res
-    .status(200)
-    .json({ status: "success", results: users.length, data: { users } });
-};
-
-exports.getSpecificUser = (req, res) => {
-  const user = users.find((el) => el.id === +req.params.id);
-
-  if (!user) {
-    return res.status(404).json({
-      status: "error",
-      message: "Invalid ID",
-    });
-  }
-  res.status(200).json({ status: "success", data: { user } });
-};
+exports.getAllUsers = factory.getAll(User);
+exports.getSpecificUser = factory.getSpecidficOne(User);
+exports.updateUser = factory.updateOne(User);
+exports.deleteUser = factory.deleteOne(User);
 
 exports.createUser = (req, res) => {
-  const newId = users[users.length - 1].id + 1;
-  const newUser = Object.assign({ id: newId }, req.body);
-
-  users.push(newUser);
-
-  fs.writeFile(
-    `${__dirname}/../data/users.json`,
-    JSON.stringify(users),
-    (err) => {
-      res.status(201).json({ status: "success", data: { user: newUser } });
-    }
-  );
-};
-
-exports.updateUser = (req, res) => {
-  if (+req.params.id > users.length) {
-    return res.status(404).json({
+  res
+    .status(500)
+    .json({
       status: "error",
-      message: "Invalid ID",
+      message: "This route is not defined. Please use /signup instead.",
     });
-  }
-
-  res.status(200).json({ status: "success", data: { user: "<Updated user>" } });
-};
-
-exports.deleteUser = (req, res) => {
-  if (+req.params.id > users.length) {
-    return res.status(404).json({
-      status: "error",
-      message: "Invalid ID",
-    });
-  }
-
-  res.status(200).json({ status: "success", data: null });
 };
