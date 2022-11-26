239ac74006aae5a3fc05a2376069b8566456083b
Helen Quang <94285120+HelenQuang@users.noreply.github.com>
2022-11-26 23:43:14 +0200

    Add security practices

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 const express = require("express");
 const morgan = require("morgan");
 const path = require("path");
+const rateLimit = require("express-rate-limit");
+const helmet = require("helmet");
+const mongoSanitize = require("express-mongo-sanitize");
+const xss = require("xss-clean");
+const cookieParser = require("cookie-parser");
+const compression = require("compression");
 
 const userRouter = require("./routes/userRoutes");
 const lotteryRouter = require("./routes/lotteryRoutes");
@@ -20,6 +26,29 @@ app.use(express.json({ limit: "10kb" }));
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
+
+//Compress text response sent to client
+app.use(compression());
+
+//To set security headers
+app.use(helmet());
+
+//To limit the requests coming from 1 IP => block these requests
+const limiter = rateLimit({
+  max: 100, //number of max requests
+  windowMs: 60 * 60 * 1000, //Milisecond time window (1hour)
+  message: "Too many requests from this IP, please try again in an hour!",
+});
+app.use("/api", limiter);
+
+//To read data from browser cookie
+app.use(cookieParser());
+
+//To sanitize data against NoSQL query injection
+app.use(mongoSanitize());
+
+//To sanitize data against XSS attacks
+app.use(xss());
 
 ////////////////ROUTES
 app.use("/api/v1/users", userRouter);
