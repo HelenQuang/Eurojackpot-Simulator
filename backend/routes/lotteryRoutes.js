9f0988885348ceaad76a6d5136aefc01816f9359
Helen Quang <94285120+HelenQuang@users.noreply.github.com>
2022-12-15 20:57:07 +0200

    Update lotteryRoutes.js

--- backend/routes/lotteryRoutes.js
+++ backend/routes/lotteryRoutes.js
@@ -8,9 +8,8 @@ const { protect, restrict } = require("../controllers/authController");
 const { createTransaction } = require("../controllers/transactionController");
 
 const router = express.Router();
+router.use(protect);
 router.route("/createTransaction").post(createTransaction);
-// router.use(protect);
-
 router.route("/").get(restrict, getAllLotteries).post(submitLottery);
 router.route("/:id").get(getSpecificLottery);
 
