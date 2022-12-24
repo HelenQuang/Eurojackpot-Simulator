996ef570104beff3419c47505eadfa16c960c2d6
Helen Quang <94285120+HelenQuang@users.noreply.github.com>
2022-12-24 13:44:46 +0200

    Update ProfilePage.tsx

--- frontend/src/pages/ProfilePage.tsx
+++ frontend/src/pages/ProfilePage.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import PaidIcon from "@mui/icons-material/Paid";
 import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 import EmailIcon from "@mui/icons-material/Email";
@@ -5,28 +6,16 @@ import AddCardIcon from "@mui/icons-material/AddCard";
 import CreditScoreIcon from "@mui/icons-material/CreditScore";
 import SportsEsportsIcon from "@mui/icons-material/SportsEsports";
 import { useNavigate } from "react-router-dom";
-import { useUserLogin } from "../hooks/userHooks";
 
 const ProfilePage = () => {
   const navigate = useNavigate();
-  const { isSuccess } = useUserLogin();
+  const userInfo = JSON.parse(localStorage.getItem("userInfo")!);
 
-  if (!isSuccess) {
-    navigate("/");
-  } else {
-    const { name, email, lotteries, gameAccount } = JSON.parse(
-      localStorage.getItem("userInfo") || ""
-    );
-  }
-
-  const totalWin = lotteries
-    .map((ticket: { prize: number }) => {
-      return ticket.prize;
-    })
-    .reduce(
-      (accumulator: number, currentValue: number) => accumulator + currentValue,
-      0
-    );
+  useEffect(() => {
+    if (!userInfo) {
+      navigate("/");
+    }
+  }, [userInfo]);
 
   return (
     <div
@@ -40,39 +29,65 @@ const ProfilePage = () => {
     >
       <h2>User Profile</h2>
 
-      <ul
-        style={{
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "flex-start",
-          gap: "0.5rem",
-        }}
-      >
-        <li style={{ display: "flex", justifyContent: "center", gap: "1rem" }}>
-          <AccountCircleIcon />
-          Name: {name}
-        </li>
-        <li style={{ display: "flex", justifyContent: "center", gap: "1rem" }}>
-          <EmailIcon />
-          Email: {email}
-        </li>
-        <li style={{ display: "flex", justifyContent: "center", gap: "1rem" }}>
-          <SportsEsportsIcon />
-          <span>Total play: {lotteries.length} games</span>
-        </li>
-        <li style={{ display: "flex", justifyContent: "center", gap: "1rem" }}>
-          <PaidIcon />
-          <span>Current account: {gameAccount} €</span>
-        </li>
-        <li style={{ display: "flex", justifyContent: "center", gap: "1rem" }}>
-          <AddCardIcon />
-          <span>Total topup: 30 €</span>
-        </li>
-        <li style={{ display: "flex", justifyContent: "center", gap: "1rem" }}>
-          <CreditScoreIcon />
-          <span>Total win: {totalWin} €</span>
-        </li>
-      </ul>
+      {userInfo && (
+        <ul
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "flex-start",
+            gap: "0.5rem",
+          }}
+        >
+          <li
+            style={{ display: "flex", justifyContent: "center", gap: "1rem" }}
+          >
+            <AccountCircleIcon />
+            Name: {userInfo.name}
+          </li>
+          <li
+            style={{ display: "flex", justifyContent: "center", gap: "1rem" }}
+          >
+            <EmailIcon />
+            Email: {userInfo.email}
+          </li>
+          <li
+            style={{ display: "flex", justifyContent: "center", gap: "1rem" }}
+          >
+            <SportsEsportsIcon />
+            <span>Total play: {userInfo.lotteries.length} games</span>
+          </li>
+          <li
+            style={{ display: "flex", justifyContent: "center", gap: "1rem" }}
+          >
+            <PaidIcon />
+            <span>Current account: {userInfo.gameAccount} €</span>
+          </li>
+          <li
+            style={{ display: "flex", justifyContent: "center", gap: "1rem" }}
+          >
+            <AddCardIcon />
+            <span>Total topup: 30 €</span>
+          </li>
+          <li
+            style={{ display: "flex", justifyContent: "center", gap: "1rem" }}
+          >
+            <CreditScoreIcon />
+            <span>
+              Total win:{" "}
+              {userInfo.lotteries
+                .map((ticket: { prize: number }) => {
+                  return ticket.prize;
+                })
+                .reduce(
+                  (accumulator: number, currentValue: number) =>
+                    accumulator + currentValue,
+                  0
+                )}{" "}
+              €
+            </span>
+          </li>
+        </ul>
+      )}
     </div>
   );
 };
