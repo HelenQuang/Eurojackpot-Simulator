68865f76d319744d29ebd9998a72262fede73e09
Helen Quang <94285120+HelenQuang@users.noreply.github.com>
2022-12-25 22:00:48 +0200

    Update TicketSelect.tsx

--- frontend/src/components/game/numbers/TicketSelect.tsx
+++ frontend/src/components/game/numbers/TicketSelect.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import { v4 as uuidv4 } from "uuid";
 
 import MainNumberTable from "./MainNumberTable";
@@ -13,14 +14,39 @@ import CachedIcon from "@mui/icons-material/Cached";
 import AddIcon from "@mui/icons-material/Add";
 
 const TicketSelect = () => {
+  const navigate = useNavigate();
   const [mainNum, setMainNum] = useState<number[]>([]);
   const [starNum, setStarNum] = useState<number[]>([]);
   const [maxMainNum, setMaxMainNum] = useState<boolean>(false);
   const [maxStarNum, setMaxStarNum] = useState<boolean>(false);
 
+  const userInfo = JSON.parse(localStorage.getItem("userInfo")!);
+
   const autoGenerateHandler = () => {
     const randomTicket = generateLottery();
-    console.log(randomTicket);
+    if (userInfo) {
+      // userInfo["lotteries"] = [...userInfo.lotteries, randomTicket];
+      console.log(userInfo.lotteries);
+      localStorage.setItem("userInfo", JSON.stringify(userInfo));
+    }
+  };
+
+  const addTicketHandler = () => {
+    const normalTicket = { mainNum: mainNum, starNum: starNum };
+    if (userInfo) {
+      userInfo["lotteries"] = normalTicket;
+      localStorage.setItem("userInfo", JSON.stringify(userInfo));
+    }
+  };
+
+  const payTicketHandler = () => {
+    if (!userInfo) {
+      navigate("/login");
+      alert("Please login before paying the lottery tickets.");
+    } else {
+      console.log(userInfo.lotteries);
+      console.log("Pay ticket");
+    }
   };
 
   return (
@@ -61,7 +87,7 @@ const TicketSelect = () => {
           type="button"
           startIcon={<AddIcon />}
           style={{ borderColor: "var(--purple)", color: "var(--purple)" }}
-          // onClick={autoGenerateHandler}
+          onClick={addTicketHandler}
         >
           Add ticket
         </Button>
@@ -85,6 +111,7 @@ const TicketSelect = () => {
           fontWeight: 600,
           fontSize: "1.2rem",
         }}
+        onClick={payTicketHandler}
       >
         Pay 20.00 €
       </Button>
