b6538f755ed6f888fad38d8e3580cf1d2ab609af
Helen Quang <94285120+HelenQuang@users.noreply.github.com>
2022-12-26 20:36:10 +0200

    Update lotteryHooks.tsx

--- frontend/src/hooks/lotteryHooks.tsx
+++ frontend/src/hooks/lotteryHooks.tsx
@@ -1,4 +1,4 @@
-import { useQuery, useMutation } from "react-query";
+import { useMutation } from "react-query";
 import axios from "axios";
 
 const createStripeTransaction = (amountId: string) => {
