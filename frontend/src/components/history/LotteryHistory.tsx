58e2cc367b77ac65f66d2db9c2926693636f736e
Helen Quang <94285120+HelenQuang@users.noreply.github.com>
2022-12-30 18:27:19 +0200

    Update LotteryHistory.tsx

--- frontend/src/components/history/LotteryHistory.tsx
+++ frontend/src/components/history/LotteryHistory.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import { useState } from "react";
 import {
   Paper,
   Box,
@@ -9,10 +9,59 @@ import {
   SelectChangeEvent,
 } from "@mui/material";
 import lotteryModel from "../../models/lotteryModel";
+import LotteryHistoryDetails from "./LotteryHistoryDetails";
 
 const LotteryHistory = ({ lotteries }: { lotteries: lotteryModel[] }) => {
-  console.log(lotteries);
-  return <div></div>;
+  let sortedLottery: lotteryModel[] = [];
+
+  const [sortBy, setSortBy] = useState("newest");
+
+  if (sortBy === "oldest") {
+    sortedLottery = [...lotteries].sort(function (a, b) {
+      return +new Date(a.playAt) - +new Date(b.playAt);
+    });
+  }
+  if (sortBy === "newest") {
+    sortedLottery = [...lotteries].sort(function (a, b) {
+      return +new Date(b.playAt) - +new Date(a.playAt);
+    });
+  }
+
+  return (
+    <div>
+      <Paper
+        elevation={6}
+        sx={{
+          backgroundColor: "var(--white)",
+          padding: "2rem",
+          // width: "45rem",
+          // marginTop: "1rem",
+        }}
+      >
+        <Box sx={{ minWidth: 120 }}>
+          <FormControl fullWidth>
+            <InputLabel id="sortBy">Sort By</InputLabel>
+            <Select
+              labelId="sortBySelect"
+              id="sortBySelect"
+              value={sortBy}
+              label="Sort By"
+              onChange={(event: SelectChangeEvent) => {
+                setSortBy(event.target.value as string);
+              }}
+            >
+              <MenuItem value={"newest"}>Newest</MenuItem>
+              <MenuItem value={"oldest"}>Oldest</MenuItem>
+            </Select>
+          </FormControl>
+        </Box>
+
+        {sortedLottery.map((lottery) => (
+          <LotteryHistoryDetails key={lottery._id} lottery={lottery} />
+        ))}
+      </Paper>
+    </div>
+  );
 };
 
 export default LotteryHistory;
