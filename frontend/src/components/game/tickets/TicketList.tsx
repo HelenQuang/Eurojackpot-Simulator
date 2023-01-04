504714ef1585ec408dabb47a42c3b1815b10d04d
Helen Quang <94285120+HelenQuang@users.noreply.github.com>
2023-01-04 16:02:02 +0200

    Update TicketList.tsx

--- frontend/src/components/game/tickets/TicketList.tsx
+++ frontend/src/components/game/tickets/TicketList.tsx
@@ -1,7 +1,21 @@
+import { Dispatch, SetStateAction } from "react";
 import TicketItem from "./TicketItem";
 import ticketModel from "../../../models/ticketModel";
+import { Button } from "@mui/material";
+import DeleteForeverIcon from "@mui/icons-material/DeleteForever";
 
-const TicketList = ({ newTickets }: { newTickets: ticketModel[] }) => {
+const TicketList = ({
+  newTickets,
+  setNewTickets,
+}: {
+  newTickets: ticketModel[];
+  setNewTickets: Dispatch<SetStateAction<ticketModel[]>>;
+}) => {
+  const deleteTicket = (id: string | undefined) => {
+    const updatedNewTickets = newTickets.filter((ticket) => ticket.id !== id);
+    setNewTickets(updatedNewTickets);
+  };
+
   return (
     <div>
       {newTickets.length === 0 && <p>Please select a ticket</p>}
@@ -10,7 +24,20 @@ const TicketList = ({ newTickets }: { newTickets: ticketModel[] }) => {
 
       {newTickets.length > 0 &&
         newTickets.map((ticket: ticketModel) => (
-          <TicketItem key={ticket._id} ticket={ticket} showIcon={true} />
+          <div
+            key={ticket.id}
+            style={{ display: "flex", flexDirection: "row" }}
+          >
+            <TicketItem ticket={ticket} />
+            <Button
+              type="button"
+              startIcon={<DeleteForeverIcon />}
+              style={{ color: "var(--dark-purple)" }}
+              onClick={() => {
+                deleteTicket(ticket.id);
+              }}
+            />
+          </div>
         ))}
     </div>
   );
