8a56892f73a3707c916bcb84137b3b5f9a57e05c
Helen Quang <94285120+HelenQuang@users.noreply.github.com>
2023-01-05 16:16:26 +0200

    Update userRoutes.js

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -10,6 +10,7 @@ const {
   deleteMe,
   uploadUserPhoto,
   resizeUserPhoto,
+  updateTransaction,
 } = require("../controllers/userController");
 const {
   signUp,
@@ -25,7 +26,7 @@ const router = express.Router();
 
 router.route("/signup").post(signUp);
 router.route("/login").post(logIn);
-// router.route("/logout").get(logOut);
+
 router.route("/forgotPassword").post(forgotPassword);
 router.route("/resetPassword/:token").patch(resetPassword);
 
@@ -34,6 +35,7 @@ router.route("/updatePassword").patch(updatePassword);
 router.route("/me").get(getMe, getSpecificUser);
 router.route("/updateMe").patch(uploadUserPhoto, resizeUserPhoto, updateMe);
 router.route("/deleteMe").delete(deleteMe);
+router.route("/updateTransaction").post(updateTransaction);
 
 // router.use(restrict);
 router.route("/").get(getAllUsers).post(createUser);
