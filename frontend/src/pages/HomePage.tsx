a523c340f8ad2d6120fa569dc88f9803dc8dbf50
Helen Quang <94285120+HelenQuang@users.noreply.github.com>
2023-01-07 23:44:38 +0200

    Update HomePage.tsx

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,13 +1,17 @@
+import { useState } from "react";
 import TicketList from "../components/game/tickets/TicketList";
 import TicketSelect from "../components/game/numbers/TicketSelect";
+import ticketModel from "../models/ticketModel";
 
 const HomePage = () => {
+  const [newTickets, setNewTickets] = useState<ticketModel[]>([]);
+
   return (
     <>
       <h2>Basic Game</h2>
       <div style={{ display: "flex", justifyContent: "space-between" }}>
-        <TicketList />
-        <TicketSelect />
+        <TicketList newTickets={newTickets} setNewTickets={setNewTickets} />
+        <TicketSelect newTickets={newTickets} setNewTickets={setNewTickets} />
       </div>
     </>
   );
