3cb3edfbfebac3f0610ba096f6b7505b40ca3271
Helen Quang <94285120+HelenQuang@users.noreply.github.com>
2023-01-12 18:08:59 +0200

    Update SignUpPage.tsx

--- frontend/src/pages/SignUpPage.tsx
+++ frontend/src/pages/SignUpPage.tsx
@@ -44,7 +44,10 @@ const SignUpPage = () => {
 
   useEffect(() => {
     if (isSuccess) {
-      localStorage.setItem("token", JSON.stringify(data.data.token));
+      localStorage.setItem(
+        "isAuthenticated",
+        JSON.stringify("isAuthenticated")
+      );
       localStorage.setItem("userInfo", JSON.stringify(data.data.data.user));
       navigate("/");
     } else if (axios.isAxiosError(error)) {
