a6a831ac579a19c33c17c059ebe9bc3972d8534d
Helen Quang <94285120+HelenQuang@users.noreply.github.com>
2023-02-03 16:15:50 +0200

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -17,7 +17,17 @@ const app = express();
 
 /////////////////MIDDLEWARES
 //Serving static files
-app.use(express.static(path.join(__dirname, "public")));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running....");
+  });
+}
 
 //To read data from body into req.body
 app.use(express.json({ limit: "10kb" }));
